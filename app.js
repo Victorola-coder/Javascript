26baa13ff9a1a1f66fc27a7f4383b0c1d5c9ec45
Victor Olatunji <victoluolatunji@gmail.com>
2023-11-04 16:03:40 +0100

    mehn

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let x;
 
-const fruits = ["orange", "pineapple", "grape", "banana"];
-const car = ["toyota", "volks", "nissan", "passat"];
+const fruits = ['orange', 'pineapple', 'grape', 'banana'];
+const car = ['toyota', 'volks', 'nissan', 'passat'];
 
 // fruits.push(car)
 
@@ -30,7 +30,7 @@ console.log(arr);
 const arr1 = [1, 2, 3, 4, 5];
 const arr2 = [5, 6, 7, 8, 9, 10];
 
-// solutin 1
+// solution 1
 
 // const arr3 = [arr1.splice(0, 5), arr2.splice(1, 5)]
 
@@ -42,57 +42,54 @@ const arr2 = [5, 6, 7, 8, 9, 10];
 
 // solution3
 
-let y = arr1.concat(arr2)
+let y = arr1.concat(arr2);
 
-let z = [...arr1, ...arr2]
+let z = [...arr1, ...arr2];
 
-z.splice(4, 1)
+z.splice(4, 1);
 
-
-console.log(z)
-
+console.log(z);
 
 // object challenge
 
 const library = [
-    {
-        id: 1,
-        title: "How to Make Heaven",
-        author: "Alex Gram",
-        status: {
-            own: true,
-            reading: false,
-            read: false
-        }
+  {
+    id: 1,
+    title: 'How to Make Heaven',
+    author: 'Alex Gram',
+    status: {
+      own: true,
+      reading: false,
+      read: false,
     },
-    {
-        id: 2,
-        title: "Javascript for Vip",
-        author: "Ebenezer Ben",
-        status: {
-            own: true,
-            reading: false,
-            read: false
-        }
+  },
+  {
+    id: 2,
+    title: 'Javascript for Vip',
+    author: 'Ebenezer Ben',
+    status: {
+      own: true,
+      reading: false,
+      read: false,
     },
-    {
-        id: 3,
-        title: "No one cares",
-        author: "Butter Bread",
-        status: {
-            own: true,
-            reading: false,
-            read: false
-        }
+  },
+  {
+    id: 3,
+    title: 'No one cares',
+    author: 'Butter Bread',
+    status: {
+      own: true,
+      reading: false,
+      read: false,
     },
-]
+  },
+];
 
 library[0].status.read = true;
 library[1].status.read = true;
 library[2].status.read = true;
 
-const { title: firstBook } = library[0]
+const { title: firstBook } = library[0];
 
-
-const json = JSON.stringify(library)
-console.log(json)
+const json = JSON.stringify(library);
+console.log(json);
