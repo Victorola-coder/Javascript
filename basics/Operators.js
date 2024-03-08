05f3fca7a377e71c222c3c303dbf7f15692fe245
Victor Olatunji <victoluolatunji@gmail.com>
2024-03-08 14:18:41 +0100

    .

---
+++ basics/Operators.js
@@ -0,0 +1,25 @@
+let x;
+
+// 1. Arithmetic
+
+x = 5 + 5; // addition - 10
+
+x = 5 - 5; // subtraction - 0
+
+x = 5 * 5; // multiplication - 25
+
+x = 5 / 5; // division - 1
+
+x = 10 % 3; // operand - handles remainder 1
+
+// 2. Concatenation - joining of two or more data type mostly string, we use the addition operator to concatenate
+
+x = "Hello" + " " + "World"; // Hello World
+
+// 3. Exponential
+
+x = 4 ** 4; // 4 raise to power 4 = 235
+
+x = 3 ** 3; // 27
+
+console.log(x);
---
+++ basics/types-conversion.js
@@ -0,0 +1,14 @@
+// type-conversion involves converting a data type to another, e.g number to string vice versa, etc
+
+// NUMBER  to STRING
+let x = 100;
+
+// let output = x.toString(); // string
+
+// convert from string to number
+
+// let output = parseInt(x); // number
+
+// convert from
+
+console.log(output, typeof output);
