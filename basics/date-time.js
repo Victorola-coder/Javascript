6ebebbebdddeccb47df9eb7ee7814e8526ee95f9
Victor Olatunji <victoluolatunji@gmail.com>
2024-03-09 20:43:10 +0100

    date

---
+++ basics/date-time.js
@@ -0,0 +1,13 @@
+// Working with Javascript Date Object
+
+let date;
+
+date = new Date();
+
+date = date.toString();
+
+date = Date.now(); //Returns the number of milliseconds elapsed since midnight, January 1, 1970 Universal Coordinated Time (UTC).
+
+date = d;
+
+console.log(date, typeof date);
