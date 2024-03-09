9a7f815a936b1eb2d88f9fdc495968369c2a9578
Victor Olatunji <victoluolatunji@gmail.com>
2024-03-09 10:10:30 +0100

    [chore]: number challenge

---
+++ challenges/number/number.js
@@ -0,0 +1,34 @@
+let x = Math.floor(Math.random() * 100 + 1); // generates random number from 1 to 100
+
+let y = Math.floor(Math.random() * 50 + 1); // generates random number between 1-50
+
+// console.log(x, y);
+
+const sum = x + y;
+const summation = `${x} + ${y} = ${sum}`;
+
+console.log(summation);
+
+const difference = x - y;
+
+const subtraction = `${x} - ${y} = ${difference}`;
+
+console.log(subtraction);
+
+const product = x * y;
+
+const multiplication = `${x} * ${y} = ${product}`;
+
+console.log(multiplication);
+
+const quotient = x / y;
+
+const division = `${x} / ${y} = ${quotient}`;
+
+console.log(division);
+
+const remainder = x % y;
+
+const modulus = `${x} % ${y} = ${remainder}`;
+
+console.log(modulus);
---
+++ challenges/number/readme.md
@@ -0,0 +1,7 @@
+# Number Challenge
+
+**Instructions:**
+
+Create a variable called `x` that is a random number between 1 and 100 along with a variable called `y` that is a random number between 1 and 50.
+
+Create a variable for the sum, difference, product, quotient and remainder of `x` and `y`. Log the output in a string that shows the two numbers of `x` and `y` along with the operator and result.
