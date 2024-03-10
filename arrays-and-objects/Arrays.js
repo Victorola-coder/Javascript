7ae1a429c0652a67fadcbf93363757a5da5c71e2
Victor Olatunji <victoluolatunji@gmail.com>
2024-03-10 10:20:33 +0100

    .

---
+++ arrays-and-objects/Arrays.js
@@ -0,0 +1,10 @@
+// Array is a tye of object in Javascript that is commonly created with  with square brackets [array literals], it can take any data type and each elements is separated with a comma, arrays can also be created with the new construct Keyword  but wrapped with a normal brackets (array object) you cn also call array DATA STRUCTURE. In many programming languages, array is zero based index, this means the first element in array is in 0 position
+
+// ARRAY LITERAL
+const arr = [374, 755, 398, 118, 289];
+
+// ARRAY CONSTRUCT
+const arrObj = new Array(374, 755, 398, 118, 289);
+
+//
+console.log(arr, typeof arr);
