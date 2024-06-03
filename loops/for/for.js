6ccd96c8251bf18bd336ddb28073617261273cff
Victor VickyJay <victoluolatunji@gmail.com>
2024-06-03 15:01:50 +0100

    .

--- loops/for/for.js
+++ loops/for/for.js
@@ -45,8 +45,6 @@ for (let i = 0; i <= 10; i++) {
 */
 
 for (let i = 1; i <= 12; i++) {
-  // console.log("number" + i);
-
   for (let j = 1; j <= 12; j++) {
     console.log(`${i} * ${j} = ${i * j}`);
   }
