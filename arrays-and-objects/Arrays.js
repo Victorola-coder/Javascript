6414bac61eecd145d7a91290a87f77d603a3b9a1
Victor || VickyJay <77025670+Victorola-coder@users.noreply.github.com>
2024-07-19 22:10:03 +0100

    [fix] typo

--- arrays-and-objects/Arrays.js
+++ arrays-and-objects/Arrays.js
@@ -1,4 +1,4 @@
-// Array is a tye of object in Javascript that is commonly created with  with square brackets [array literals], it can take any data type and each elements is separated with a comma, arrays can also be created with the new construct Keyword  but wrapped with a normal brackets (array object) you cn also call array DATA STRUCTURE. In many programming languages, array is zero based index, this means the first element in array is in 0 position
+// Array is a type of object in Javascript that is commonly created with  with square brackets [array literals], it can take any data type and each elements is separated with a comma, arrays can also be created with the new construct Keyword  but wrapped with a normal brackets (array object) you cn also call array DATA STRUCTURE. In many programming languages, array is zero based index, this means the first element in array is in 0 position
 
 let output;
 
